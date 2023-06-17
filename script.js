2750806e49f6f79b7d12a7cc7bbb86206f1e5590
souvik500 <120314952+souvik500@users.noreply.github.com>
2023-06-17 12:47:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+function rotateLine() {
+  var line = document.getElementById("line");
+  var rotation = 0;
+  
+  function animateRotation() {
+    rotation += 1;
+    line.style.transform = "rotate(" + rotation + "deg)";
+    requestAnimationFrame(animateRotation);
+  }
+  
+  animateRotation();
+}
+
+// Call the rotateLine function to rotate the line
+rotateLine();
